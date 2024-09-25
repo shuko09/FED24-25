697de2d678dd8d0b8bc1ab7a73f68d2d49e6e6c7
shuko09 <153842980+shuko09@users.noreply.github.com>
2024-09-25 11:15:32 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
 // JavaScript Document
 console.log("hi")
 
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("hi");
+  
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+  
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    }
+  });
+  
